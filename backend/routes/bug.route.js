93a813705646199094d2615a15926f70182b34ba
Sujal Bhardwaj <bhardwaj0601@gmail.com>
2026-03-28 18:48:47 +0530

    Add JWT middleware to protect bug routes

--- backend/routes/bug.route.js
+++ backend/routes/bug.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { createBug, deleteBug, getBugs, updateBug } from "../controllers/bug.controller.js";
+import protect from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/", createBug);
-router.get("/", getBugs);
-router.put("/:id", updateBug);
-router.delete("/:id", deleteBug);
+router.post("/",protect, createBug);
+router.get("/", protect, getBugs);
+router.put("/:id",protect, updateBug);
+router.delete("/:id",protect, deleteBug);
 
 export default router;
