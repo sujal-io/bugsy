9c08dde3a086c1b0df4b1d840cebb1a0bbcb06b2
Sujal Bhardwaj <bhardwaj0601@gmail.com>
2026-04-05 16:35:38 +0530

    Implement join team API

--- backend/controllers/team.controller.js
+++ backend/controllers/team.controller.js
@@ -38,7 +38,54 @@ export const createTeam = async (req, res) => {
       message: "Team created successfully",
       team: populatedTeam,
     });
-    
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const joinTeam = async (req, res) => {
+  try {
+    const { inviteCode } = req.body;
+
+    // find user
+    const user = await User.findById(req.user.id);
+
+    // check if already in a team
+    if (user.team) {
+      return res.status(400).json({
+        message: "User already in a team",
+      });
+    }
+
+    // find team by invite code
+    const team = await Team.findOne({ inviteCode });
+
+    if (!team) {
+      return res.status(404).json({
+        message: "Invalid invite code",
+      });
+    }
+
+    // add user to team members
+    team.members.push(req.user.id);
+    await team.save();
+
+    // assign team to user
+    user.team = team._id;
+    await user.save();
+
+    // opulate for response
+    const populatedTeam = await Team.findById(team._id)
+      .populate("admin", "username email")
+      .populate("members", "username email");
+
+    res.status(200).json({
+      message: "Joined team successfully",
+      team: populatedTeam,
+    });
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
--- backend/routes/team.route.js
+++ backend/routes/team.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createTeam } from "../controllers/team.controller.js";
+import { createTeam, joinTeam } from "../controllers/team.controller.js";
 import protect from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/create", protect, createTeam);
+router.post("/join", protect, joinTeam);
 
 export default router;
